4f9e9279cfc1376123b5dee77e33d4e905ff9e33
Alan Dixon <alan.g.dixon@gmail.com>
2019-05-31 11:52:04 -0400

    update build script for civicrm-5 branch

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,5 @@
 # 0. slug value
 REPOSITORY_FROM='';
-docker build 6/apache -t drupal:6-apache
-docker build 7/apache -t drupal:6-apache
-docker build 8.7/apache -t drupal:8-apache
+docker build 6/apache -t drupal:6-apache-civicrm-5
+docker build 7/apache -t drupal:7-apache-civicrm-5
+docker build 8.7/apache -t drupal:8-apache-civicrm-5
