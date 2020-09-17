f61b2b7a1389b172e5026f765c0710a189462409
Alan Dixon <alan.g.dixon@gmail.com>
2020-09-17 13:39:21 -0400

    update version of default drupal 8 container

--- build-all.sh
+++ build-all.sh
@@ -2,4 +2,4 @@
 REPOSITORY_FROM='';
 docker build 6/apache -t drupal:6-apache-civicrm-5
 docker build 7/apache -t drupal:7-apache-civicrm-5
-docker build 8.8/apache -t drupal:8-apache-civicrm-5
+docker build 8.9/apache-buster -t drupal:8-apache-civicrm-5
