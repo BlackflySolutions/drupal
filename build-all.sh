c9f9a6f1d00279e3a14cfc0b25a89fa46f0fa2ab
Alan Dixon <alan.g.dixon@gmail.com>
2020-11-19 17:11:23 -0500

    use 8.9 for drupal 8

--- build-all.sh
+++ build-all.sh
@@ -2,4 +2,4 @@
 REPOSITORY_FROM='';
 docker build 6/apache -t drupal:6-apache
 docker build 7/apache -t drupal:7-apache
-docker build 8.8/apache -t drupal:8-apache
+docker build 8.9/apache -t drupal:8-apache
