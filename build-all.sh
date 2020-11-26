2d788378d0d7c7c25f4a192a70d9358a2743640d
Alan Dixon <alan.g.dixon@gmail.com>
2020-11-26 13:11:44 -0500

    restore --no-cache on build

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,6 @@
 # 0. slug value
 REPOSITORY_FROM='';
-docker build 6/apache -t drupal:6-apache-civicrm-5
-docker build 7/apache -t drupal:7-apache-civicrm-5
-docker build 7/apache-php56 -t drupal:7-apache-php56-civicrm-5
-docker build 8.9/apache-buster -t drupal:8-apache-civicrm-5
+docker build --no-cache 6/apache -t drupal:6-apache-civicrm-5
+docker build --no-cache 7/apache -t drupal:7-apache-civicrm-5
+docker build --no-cache 7/apache-php56 -t drupal:7-apache-php56-civicrm-5
+docker build --no-cache 8.9/apache-buster -t drupal:8-apache-civicrm-5
