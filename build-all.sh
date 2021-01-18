7447d5afc02e8a5fec25a22308ac425f27d2089a
Alan Dixon <alan.g.dixon@gmail.com>
2021-01-18 17:05:37 -0500

    added d9 platform using 9.0

--- build-all.sh
+++ build-all.sh
@@ -4,3 +4,4 @@ docker build --no-cache 6/apache -t drupal:6-apache-civicrm-5
 docker build --no-cache 7/apache -t drupal:7-apache-civicrm-5
 docker build --no-cache 7/apache-php56 -t drupal:7-apache-php56-civicrm-5
 docker build --no-cache 8.9/apache-buster -t drupal:8-apache-civicrm-5
+docker build --no-cache 9.0/apache-buster -t drupal:9-apache-civicrm-5
