8378ab7d9664d34bd15176db00dc2a92f3c75415
Alan Dixon <alan.g.dixon@gmail.com>
2021-11-18 10:44:57 -0500

    add convenience scripts

---
+++ push-all.sh
@@ -0,0 +1,7 @@
+# 0. slug value
+REPOSITORY_FROM='';
+docker push blackflysolutions/drupal:6-apache-civicrm-5 
+docker push blackflysolutions/drupal:7-apache-civicrm-5
+docker push blackflysolutions/drupal:7-apache-php56-civicrm-5
+docker push blackflysolutions/drupal:8-apache-civicrm-5
+docker push blackflysolutions/drupal:9-apache-civicrm-5
---
+++ tag-all.sh
@@ -0,0 +1,7 @@
+# 0. slug value
+REPOSITORY_FROM='';
+docker tag drupal:6-apache-civicrm-5 blackflysolutions/drupal:6-apache-civicrm-5 
+docker tag drupal:7-apache-civicrm-5 blackflysolutions/drupal:7-apache-civicrm-5
+docker tag drupal:7-apache-php56-civicrm-5 blackflysolutions/drupal:7-apache-php56-civicrm-5
+docker tag drupal:8-apache-civicrm-5 blackflysolutions/drupal:8-apache-civicrm-5
+docker tag drupal:9-apache-civicrm-5 blackflysolutions/drupal:9-apache-civicrm-5
