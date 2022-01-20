8d9974f1d5436778957704b0b7b6456e6acd1a8c
Alan Dixon <alan.g.dixon@gmail.com>
2022-01-20 11:53:55 -0500

    updated build-all.sh to drop 8.9 and change 9 tag to 9.2

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,6 @@
 # 0. slug value
 REPOSITORY_FROM='';
-docker build 6/apache -t drupal:6-apache-civicrm-5
+# docker build 6/apache -t drupal:6-apache-civicrm-5
 docker build 7/apache -t drupal:7-apache-civicrm-5
 docker build 7/apache-php56 -t drupal:7-apache-php56-civicrm-5
-docker build 8.9/php7.4/apache-buster -t drupal:8-apache-civicrm-5
-docker build 9.1/php7.4/apache-buster -t drupal:9-apache-civicrm-5
+docker build 9.2/php7.4/apache-buster -t drupal:9-apache-civicrm-5
