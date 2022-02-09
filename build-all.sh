66cdd5837d0ab0bec35425182ce798e24f0d5aed
Alan Dixon <alan.g.dixon@gmail.com>
2022-02-09 13:06:23 -0500

    use php7.4/apache-buster for d7

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,6 @@
 # 0. slug value
 REPOSITORY_FROM='';
 # docker build 6/apache -t drupal:6-apache-civicrm-5
-docker build 7/apache -t drupal:7-apache-civicrm-5
+docker build 7/php7.4/apache-buster -t drupal:7-apache-civicrm-5
 docker build 7/apache-php56 -t drupal:7-apache-php56-civicrm-5
 docker build 9.3/php7.4/apache-buster -t drupal:9-apache-civicrm-5
