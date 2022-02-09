08dbf9363bae546df639a09ea1f16e5358515040
Alan Dixon <alan.g.dixon@gmail.com>
2022-02-09 09:26:17 -0500

    use php7.4 + 9.3 as the base image for D9

--- build-all.sh
+++ build-all.sh
@@ -3,4 +3,4 @@ REPOSITORY_FROM='';
 # docker build 6/apache -t drupal:6-apache-civicrm-5
 docker build 7/apache -t drupal:7-apache-civicrm-5
 docker build 7/apache-php56 -t drupal:7-apache-php56-civicrm-5
-docker build 9.2/php7.4/apache-buster -t drupal:9-apache-civicrm-5
+docker build 9.3/php7.4/apache-buster -t drupal:9-apache-civicrm-5
