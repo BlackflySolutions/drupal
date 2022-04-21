1ad7f8ca564bb4ba22fea43b600ac944485cbe1e
Alan Dixon <alan.g.dixon@gmail.com>
2022-04-21 09:25:02 -0400

    drop support for drupal 8 and drupal 7 + php5.6

--- push-all.sh
+++ push-all.sh
@@ -2,6 +2,6 @@
 REPOSITORY_FROM='';
 docker push blackflysolutions/drupal:6-apache-civicrm-5 
 docker push blackflysolutions/drupal:7-apache-civicrm-5
-docker push blackflysolutions/drupal:7-apache-php56-civicrm-5
-docker push blackflysolutions/drupal:8-apache-civicrm-5
+#docker push blackflysolutions/drupal:7-apache-php56-civicrm-5
+#docker push blackflysolutions/drupal:8-apache-civicrm-5
 docker push blackflysolutions/drupal:9-apache-civicrm-5
