91bd9be2b6d8e3e7dd28dc90f49efc4ca35ee173
Alan Dixon <alan.g.dixon@gmail.com>
2022-06-23 09:58:54 -0400

    added 10.0-rc to standard builds

--- build-all.sh
+++ build-all.sh
@@ -4,3 +4,4 @@ REPOSITORY_FROM='';
 docker build 7/php7.4/apache-buster -t drupal:7-apache-civicrm-5
 #docker build 7/apache-php56 -t drupal:7-apache-php56-civicrm-5
 docker build 9.3/php7.4/apache-buster -t drupal:9-apache-civicrm-5
+docker build 10.0-rc/php8.1/apache-buster -t drupal:10-apache-civicrm-5
