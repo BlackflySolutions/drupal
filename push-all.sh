70d6cfa012700ce83673f7103cac88a75da161b3
Alan Dixon <alan.g.dixon@gmail.com>
2022-07-20 14:35:02 -0400

    remove old drupal versions from push/tag-all scripts

--- push-all.sh
+++ push-all.sh
@@ -1,7 +1,8 @@
 # 0. slug value
 REPOSITORY_FROM='';
-docker push blackflysolutions/drupal:6-apache-civicrm-5 
+#docker push blackflysolutions/drupal:6-apache-civicrm-5 
 docker push blackflysolutions/drupal:7-apache-civicrm-5
 #docker push blackflysolutions/drupal:7-apache-php56-civicrm-5
 #docker push blackflysolutions/drupal:8-apache-civicrm-5
 docker push blackflysolutions/drupal:9-apache-civicrm-5
+docker push blackflysolutions/drupal:10-apache-civicrm-5
--- tag-all.sh
+++ tag-all.sh
@@ -1,7 +1,8 @@
 # 0. slug value
 REPOSITORY_FROM='';
-docker tag drupal:6-apache-civicrm-5 blackflysolutions/drupal:6-apache-civicrm-5 
+#docker tag drupal:6-apache-civicrm-5 blackflysolutions/drupal:6-apache-civicrm-5 
 docker tag drupal:7-apache-civicrm-5 blackflysolutions/drupal:7-apache-civicrm-5
-docker tag drupal:7-apache-php56-civicrm-5 blackflysolutions/drupal:7-apache-php56-civicrm-5
-docker tag drupal:8-apache-civicrm-5 blackflysolutions/drupal:8-apache-civicrm-5
+#docker tag drupal:7-apache-php56-civicrm-5 blackflysolutions/drupal:7-apache-php56-civicrm-5
+#docker tag drupal:8-apache-civicrm-5 blackflysolutions/drupal:8-apache-civicrm-5
 docker tag drupal:9-apache-civicrm-5 blackflysolutions/drupal:9-apache-civicrm-5
+docker tag drupal:10-apache-civicrm-5 blackflysolutions/drupal:10-apache-civicrm-5
