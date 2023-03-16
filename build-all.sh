ff5a5aebbb9f23f7f86e664357ff90b2784e5ae4
Alan Dixon <alan.g.dixon@gmail.com>
2023-03-16 15:13:13 -0400

    update which versions are used

--- build-all.sh
+++ build-all.sh
@@ -3,5 +3,5 @@ REPOSITORY_FROM='';
 # docker build 6/apache -t drupal:6-apache-civicrm-5
 docker build 7/php7.4/apache-buster -t drupal:7-apache-civicrm-5
 #docker build 7/apache-php56 -t drupal:7-apache-php56-civicrm-5
-docker build 9.3/php7.4/apache-buster -t drupal:9-apache-civicrm-5
-docker build 10.0-rc/php8.1/apache-buster -t drupal:10-apache-civicrm-5
+docker build 9.4/php7.4/apache-buster -t drupal:9-apache-civicrm-5
+docker build 10.0/php8.1/apache-buster -t drupal:10-apache-civicrm-5
